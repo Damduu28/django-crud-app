d62d2ff98a6e6f03d233242d656953775356de94
Damduu28 <sulemed26@gmail.com>
2023-12-18 09:37:55 +0000

    commit #11

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 # build_files.sh
 pip install -r requirements.txt
 
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
 
 
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic
